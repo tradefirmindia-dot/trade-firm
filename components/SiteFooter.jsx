f4a637f12740ac9e3e01cc5bfd66ca0115828cfc
tradefirmindia-dot <tradefirmindia@gmail.com>
2026-08-21 14:14:25 +0530

    Add forex page to footer navigation

--- components/SiteFooter.jsx
+++ components/SiteFooter.jsx
@@ -17,6 +17,7 @@ export default function SiteFooter() {
 
         <div className="footer-column">
           <b>Advisory &amp; Research</b>
+          <Link href="/forex-broker-india">Forex Broker &amp; MT5 India</Link>
           <Link href="/stock-market-advisory-india">Stock Market Advisory India</Link>
           <Link href="/stock-market-research-india">Stock Market Research India</Link>
           <Link href="/nifty-bank-nifty-advisory">NIFTY &amp; BANK NIFTY</Link>
